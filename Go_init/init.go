7f44437ff40a92b75f7ac3cfef1b7204c9f3afea
Hema <mshemadarshini@gmail.com>
2019-01-28 15:47:07 +0800

    greetings
    greetings

---
+++ Go_init/init.go
@@ -0,0 +1,10 @@
+package main 
+import "fmt"
+
+var greetings="Hello everyone, thanks for visiting tbis profile at github. I would be uploading more go project for your view."
+
+func main() {
+	fmt.Println("\n Whatsapp People !!!")
+	fmt.Println(greetings)
+}
+
